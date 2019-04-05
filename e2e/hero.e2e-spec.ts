ee2bbf0ac0e9c8f1d311f6c7ec0e251736990d0d
bdjones2410 <brandon.jones@blackbaud.com>
2019-04-05 17:55:56 -0400

    rework visual tests

--- e2e/hero.e2e-spec.ts
+++ e2e/hero.e2e-spec.ts
@@ -5,56 +5,56 @@ import {
 
 describe('Hero', () => {
   it('should match previous default hero screenshot', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/default');
     SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#sky-hero-default')
+    SkyHostBrowser.scrollTo('#sky-hero-default');
     expect('#sky-hero-default').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-lg'
+      screenshotName: 'sky-hero-default-lg'
     });
   });
 
   it('should match previous default hero screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/default');
     SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#sky-hero-default')
+    SkyHostBrowser.scrollTo('#sky-hero-default');
     expect('#sky-hero-default').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-xs'
+      screenshotName: 'sky-hero-default-xs'
     });
   });
 
   it('should match previous hero alternate colors screenshot', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/alternate_colors');
     SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#sky-hero-alternate-colors')
+    SkyHostBrowser.scrollTo('#sky-hero-alternate-colors');
     expect('#sky-hero-alternate-colors').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-lg'
+      screenshotName: 'sky-hero-alternate-lg'
     });
   });
 
   it('should match previous hero alternate colors screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/alternate_colors');
     SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#sky-hero-alternate-colors')
+    SkyHostBrowser.scrollTo('#sky-hero-alternate-colors');
     expect('#sky-hero-alternate-colors').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-xs'
+      screenshotName: 'sky-hero-alternate-xs'
     });
   });
 
   it('should match previous darker opacity hero screenshot', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/darker_opacity');
     SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#sky-hero-darker-opacity')
+    SkyHostBrowser.scrollTo('#sky-hero-darker-opacity');
     expect('#sky-hero-darker-opacity').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-lg'
+      screenshotName: '#sky-hero-darker-opacity-lg'
     });
   });
 
   it('should match previous darker opacity hero screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/hero');
+    SkyHostBrowser.get('visual/hero/darker_opacity');
     SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#sky-hero-darker-opacity')
+    SkyHostBrowser.scrollTo('#sky-hero-darker-opacity');
     expect('#sky-hero-darker-opacity').toMatchBaselineScreenshot(done, {
-      screenshotName: 'hero-xs'
+      screenshotName: '#sky-hero-darker-opacity-xs'
     });
   });
 });
--- src/app/visual/hero/hero-visual.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sky-hero-visual',
-  templateUrl: './hero-visual.component.html'
-})
-export class SkyHeroTestComponent {}
