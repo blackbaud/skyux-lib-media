0ce8924c452b5786f407713ad5ffdf25ca82d641
Steve Brush <steve.brush@blackbaud.com>
2019-05-08 09:59:37 -0400

    Cleaned up image component visual tests. (#5)

--- e2e/image.e2e-spec.ts
+++ e2e/image.e2e-spec.ts
@@ -4,129 +4,133 @@ import {
 } from '@skyux-sdk/e2e';
 
 describe('Image', () => {
-  it('should match previous default caption screenshot', (done) => {
+  beforeEach(() => {
     SkyHostBrowser.get('visual/image');
+  });
+
+  it('should match previous default caption screenshot', (done) => {
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-default-caption');
+
     expect('#sky-image-default-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'default-caption-lg'
+      screenshotName: 'image-default-caption-lg'
     });
   });
 
   it('should match previous default caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-default-caption');
+
     expect('#sky-image-default-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'default-caption-xs'
+      screenshotName: 'image-default-caption-xs'
     });
   });
 
   it('should match previous success caption screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-success-caption');
+
     expect('#sky-image-success-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'success-caption-lg'
+      screenshotName: 'image-success-caption-lg'
     });
   });
 
   it('should match previous success caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-success-caption');
+
     expect('#sky-image-success-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'success-caption-xs'
+      screenshotName: 'image-success-caption-xs'
     });
   });
 
   it('should match previous danger caption screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-danger-caption');
+
     expect('#sky-image-danger-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'danger-caption-lg'
+      screenshotName: 'image-danger-caption-lg'
     });
   });
 
   it('should match previous danger caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-danger-caption');
+
     expect('#sky-image-danger-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'danger-caption-xs'
+      screenshotName: 'image-danger-caption-xs'
     });
   });
 
   it('should match previous warning caption screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-warning-caption');
+
     expect('#sky-image-warning-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'warning-caption-lg'
+      screenshotName: 'image-warning-caption-lg'
     });
   });
 
   it('should match previous warning caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-warning-caption');
+
     expect('#sky-image-warning-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'warning-caption-xs'
+      screenshotName: 'image-warning-caption-xs'
     });
   });
 
   it('should match previous info caption screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-info-caption');
+
     expect('#sky-image-info-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'info-caption-lg'
+      screenshotName: 'image-info-caption-lg'
     });
   });
 
   it('should match previous info caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-info-caption');
+
     expect('#sky-image-info-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'info-caption-xs'
+      screenshotName: 'image-info-caption-xs'
     });
   });
 
   it('should match previous show border screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-show-border');
+
     expect('#sky-image-show-border').toMatchBaselineScreenshot(done, {
-      screenshotName: 'show-border-lg'
+      screenshotName: 'image-show-border-lg'
     });
   });
 
   it('should match previous show border screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-show-border');
+
     expect('#sky-image-show-border').toMatchBaselineScreenshot(done, {
-      screenshotName: 'show-border-xs'
+      screenshotName: 'image-show-border-xs'
     });
   });
 
   it('should match previous no caption screenshot', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('lg');
     SkyHostBrowser.scrollTo('#sky-image-no-caption');
+
     expect('#sky-image-no-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'no-caption-lg'
+      screenshotName: 'image-no-caption-lg'
     });
   });
 
   it('should match previous no caption screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/image');
     SkyHostBrowser.setWindowBreakpoint('xs');
     SkyHostBrowser.scrollTo('#sky-image-no-caption');
+
     expect('#sky-image-no-caption').toMatchBaselineScreenshot(done, {
-      screenshotName: 'no-caption-xs'
+      screenshotName: 'image-no-caption-xs'
     });
   });
 });
